7cae4494c94d3aac30e90f1937a252713998eca7
Martí Adrogué <marti.adrogue@gmail.com>
2016-08-30 19:36:23 +0200

    Adapt lamp module

--- puppet/modules/lamp/manifests/init.pp
+++ puppet/modules/lamp/manifests/init.pp
@@ -1,13 +1,14 @@
 
 class lamp(
-    $vhost = 'vhost',
+    $vhost = $title,
+    $www_root = '/var/www',
     $public = 'public',
     $root_password = '12345',
     $db_password = '12345',
   ) {
     include php
-    include apache
-    apache::vhost { vhost: }
+    class { 'apache': www_root => $www_root}
+    apache::vhost { $vhost: public => $public }
     class { 'mariadb': root_password => $root_password }
     mariadb::db::create { 'logindb': password => $db_password }
 }
