fac9254bb203e7022c3a6cfb4e9af1a71419c2c5
Martí Adrogué <marti.adrogue@gmail.com>
2017-10-08 17:15:41 +0200

    Remove permissions issue

--- puppet/modules/timezone/manifests/init.pp
+++ puppet/modules/timezone/manifests/init.pp
@@ -22,32 +22,4 @@ class timezone {
     target => '/usr/share/zoneinfo/Europe/Madrid',
     subscribe => Exec['rm_timezone']
   }
-
-  exec { 'ntpd_stop':
-    path    => "/sbin/",
-    command => 'service ntpd stop',
-    require => Package['ntp'],
-    before  => Exec['synchronize_clock'],
-  }
-
-  exec { 'synchronize_clock':
-    path    => "/sbin/",
-    command => 'ntpdate 1.es.pool.ntp.org', # 2.europe.pool.ntp.org 3.europe.pool.ntp.org
-    before => Exec['change_hardware_clock'],
-  }
-
-  exec { 'change_hardware_clock':
-    path => '/sbin/',
-    command => 'hwclock --systohc',
-  }
-
-  service { 'ntpd_start':
-    name => 'ntpd',
-    ensure => running,
-    enable => true,
-    hasrestart => true,
-    hasstatus => true,
-    subscribe  => Exec['change_hardware_clock'],
-  }
-
 }
